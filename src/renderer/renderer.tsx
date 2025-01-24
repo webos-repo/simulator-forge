3d2856a0792b5db86c346d43139869b8d06cf3d5
loegnah <loegnah@gmail.com>
2025-01-24 17:24:29 +0900

    chore(ts0): Apply ts config paths plugin

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,10 +1,15 @@
+import "./global.css";
+import App from "@/renderer/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
--- vite.main.config.mts
+++ vite.main.config.mts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+});

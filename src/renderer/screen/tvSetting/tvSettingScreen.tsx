2c5e653647e606d80390dbbc3ffb9607f76b34a3
hangeol.park <hangeol.park@lge.com>
2025-02-20 19:28:41 +0900

    chore(pkg): Remove unused packages

--- src/renderer/screen/tvSetting/tvSettingScreen.tsx
+++ src/renderer/screen/tvSetting/tvSettingScreen.tsx
@@ -1,35 +1,10 @@
-import { useState, useCallback, useMemo } from "react";
-import Select from "react-select";
 import { ipcSender } from "@/renderer/lib/utils";
 
 export default function TvSettingScreen() {
-  const models = useMemo(
-    () => [
-      { value: "o20n", label: "O20N" },
-      { value: "e60n", label: "E60N" },
-      { value: "k7lp", label: "K7LP" },
-      { value: "lm21an", label: "LM21AN" },
-    ],
-    [],
-  );
-
-  const [model, setModel] = useState();
-  const handleModelChange = useCallback((inputValue: any) => {
-    setModel(inputValue);
-  }, []);
-
   return (
     <div className="tv-setting-screen">
       <div className="optionDiv">
         <span className="optionLabel">TV Model</span>
-        <Select
-          isSearchable
-          className="optionCol"
-          placeholder="TV Model"
-          value={model}
-          options={models}
-          onChange={handleModelChange}
-        />
       </div>
       <p />
       <div className="optionDiv">

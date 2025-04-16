ae215ccdd7822fb58e629bcb019b04a0b4ecceb0
hangeol.park <hangeol.park@lge.com>
2025-04-16 13:22:03 +0900

    chore(): Move isDevBranch position

--- simul.config.ts
+++ simul.config.ts
@@ -1,9 +1,10 @@
 const webOSTVVersion = "25";
 const version = "0.0.1";
+const isDevBranch = true;
 
 export const simulConfig = {
   name: `webos-tv-${webOSTVVersion}-simulator`,
   webOSTVVersion,
   version,
-  isDevBranch: true,
+  isDevBranch,
 } as const;
